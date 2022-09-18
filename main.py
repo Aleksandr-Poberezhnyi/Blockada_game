13cec5bec621f8ffc71d5d6693d2be0d6fce6323
Dedyshka_95 <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-09-18 17:06:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,12 +34,37 @@ class Button():# sania
             win.blit(self.image,(self.rect.x self.rect.y))
             win.blit(self.txt_image, (self.rect.x + shift_x, self.rect.y + shift_y))        
         
+ class Player(Settings): #?
+    
+    def r_l(self):
+        global mana, img, f
+        f = 1
+        keys = key.get_pressed()
+        if keys [K_a]:
+            self.rect.x -= self.speed
+            f = 1
+            mana.side = 'left'
+        if keys [K_d]:
+            self.rect.x += self.speed
+            mana.side = 'right' 
+            f = 0
         
+        if f ==1:
+            self.image = transform.scale(image.load(hero_r), (self.width, self.height))
+        if f ==0:
+            self.image = transform.scale(image.load(hero_l), (self.width, self.height))
+
+    def u_d(self):
+        keys = key.get_pressed()
+        if keys[K_w]:
+            self.rect.y -= self.speed
+        if keys [K_s]:
+            self.rect.y += self.speed       
      
             
             
             
-class Enemy(Settings): 
+class Enemy(Settings): #?
     def __init__(self, x, y, w, h, speed, img, side):
         Settings.__init__(self, x, y, w, h, speed, img)
         
@@ -52,6 +77,8 @@ class Enemy(Settings):
             self.rect.x -= self.speed
         if self.side == 'left':
             self.rect.x += self.speed
+            
+            
 
 widght = 1280
 height = 720
