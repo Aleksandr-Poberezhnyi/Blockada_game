2cd96c54a6afe9f0c0439d61c32ae19acb36e272
Dedyshka_95 <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-09-18 16:51:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import*
-class Settings(sprite.Sprite):
+class Settings(sprite.Sprite): # eto sania
     def __init__(self,x,y,w,h,speed,img):
         super().__init__()
 
@@ -10,6 +10,29 @@ class Settings(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        
+  class Button(): # eto sania
+        def __init__(self,color,x,y,w,h,text,fsize,txt_color):
+
+            self.width = w
+            self.heaight = h
+            self.color = color
+
+            self.image = Surface([self.width, self.height])
+            self.image.fill((color))
+
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+
+            self.fsize = fsize
+            self.text = text
+            self.txt_color = txt_color
+            self.txt_image = font.Font('font/impact.tff',fsize).render(text, True,txt_color)
+
+        def draw(self,shift_x,shift_y):#paint button with text in middle, text moved with shift_x and shift_y
+            win.blit(self.image,(self.rect.x self.rect.y))
+            win.blit(self.txt_image, (self.rect.x + shift_x, self.rect.y + shift_y))
 
 
 widght = 1280
