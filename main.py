4ee7402212a2e8ed1991863cc62085bd05b67566
dabryboy <112767864+dabryboy@users.noreply.github.com>
2022-09-18 17:17:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,47 @@ FPS = 60
 clock = time.Clock()
 game = True
 
+x=y=0
+
+for r in level:
+    for c in r:
+        if c == 'r':
+            r1 = Settings(x, y, 40, 40,0, nothing)
+            blocks_r.append(r1)
+            items.add(r1)
+
+        if c == 'l':
+            r1 = Settings(x, y, 40, 40,0, nothing)
+            blocks_l.append(r1)
+            items.add(r1)
+
+        if c == '/':
+            r2 = Settings(x, y - 40, 40, 180, 0, stair)
+            stairs.append(r2)
+            items.add(r2)
+
+        if c == '':
+            r1 = Settings(x, y, 40, 40,0, coin)
+            coins.append(r3)
+            items.add(r3)
+
+        if c == '*':
+            r4 = Settings(x,y, 40,40 , 0, portal)
+            items.add(r4)
+
+        if c == '-':
+            r5 = Settings(x, y, 40, 40 ,0 , platform)
+            platforms.append(r5)
+            items.add(r5)
+
+        if c == '>':
+            r6 = Settings(x, y - 40, 80, 80, 0, chest_close)
+            items.add(r6)
+
+        x +=40
+    y+=40
+    x = 0
+
 while game:
     for e in event.get():
         if e.type == QUIT:
