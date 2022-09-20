34bfdaa9aa485d5e6286a7b07b3bd2f945f55e97
tpashen <81863630+tpashen@users.noreply.github.com>
2022-09-20 20:35:00 +0300

    Update main.py
    перенесла переменные для картинок

--- main.py
+++ main.py
@@ -106,7 +106,23 @@ class Enemy(Settings): #nik
             self.rect.x += self.speed
             
             
+hero_r = "images/sprite1_r.png"
+hero_l = "images/sprite1.png"
 
+enemy_l = "images/cyborg.png"
+enemy_r = "images/cyborg_r.png"
+
+coin = "images/coin.png"
+door_img = "images/door.png"
+key_img = "images/key.png"
+chest_open = "images/cst_open.png"
+chest_close = "images/cst_close.png"
+cyborg = "images/cyborg.png"
+stair = "images/stair.png"
+port = "images/portal.png"
+platform = "images/platform.png"
+nothing = "images/nothing.png"
+power = "images/mana.png"
 widght = 1280
 height = 720
 
