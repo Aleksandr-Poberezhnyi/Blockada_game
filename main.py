2fc7bbf54da4f3db8f197852d7f19eb149cc6aac
dabryboy <112767864+dabryboy@users.noreply.github.com>
2022-09-25 16:57:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -231,6 +231,17 @@ while game:
         if e.type == QUIT:
             game = False
     
+    coin_c = font2.render(': ' + str(c_count), True, (255, 255, 255))
+    win.blit(transform.scale(image.load('image/coin.png')), (50,50), (10, 10))
+    win.blit(coin_c, (55, 15))
+
+    for c in coins:
+        if sprite.collide_rect(hero, c):
+            c_coll.play()
+            c_count += 1
+            coins.remove(c)
+            items.remove(c)
+    
     en1.update()
     en2.update()
 
