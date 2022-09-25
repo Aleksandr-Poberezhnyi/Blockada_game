564518218b4be38b7908d74d8ce7e9ace952cd3b
dabryboy <112767864+dabryboy@users.noreply.github.com>
2022-09-25 16:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -204,10 +204,33 @@ for r in level:
     y+=40
     x = 0
 
+    
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    
+    if sprite.collide_rect(hero, chest) and k_chest == False:
+        win.blit(k_need, (450, 50))
+
+    if sprite.collide_rect(hero, chest) and k_chest == True and c_count != 15:
+        win.blit(e_tap, (500, 50))
+        if keys[K_e]:
+            o_chest = True
+            c_count += 10
+            chest.image = transform.scale(image.load(chest_open), (chest.width, chest.height))
+            cst_o.play()
+            k_door = True
+
+    if sprite.collide_rect(hero, portal1):
+                tp.play()
+                game = False
+
+    camera.update(hero)
+    for i in items:
+        win.blit(i.image, camera.apply(i))
+
+
     
     display.update()
     clock.tick(FPS)
