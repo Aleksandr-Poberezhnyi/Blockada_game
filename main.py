6b172d4648d7541b8a51abce686cf9a61201eddb
dabryboy <112767864+dabryboy@users.noreply.github.com>
2022-10-09 16:55:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -169,8 +169,26 @@ btn_restart = Button((178, 34 , 34),470,450,280,70,"restart the game",50,(255,25
 btn_continue = Button((178, 34 , 34),470,450,280,70,"continue the game",50,(255,255,255))
 btn_pause = Button((178, 34 , 34),1200,15,50,50,"I I",40,(255,255,255))
 
+#текст в игре(пауза, вы проинарали и далее...)
+font.init()
 
+font1 = font.SysFont(('font/ariblk.ttf'), 200)
+gname= font1.render('Blockada', True, (106, 90, 205), (250, 235, 215))
 
+font2 = font.SysFont(('font/ariblk.ttf'), 60)
+e_tap =font2.render('press (e)', True, (255,0,255))
+k_need = font2.render('You need a key to open!', True, (255, 0 , 255))
+space = font2.render('press (space) to kill the enemy', True, (255,0,255))
+
+font3 = font.SysFont(('font/calibrib.ttf'), 45)
+wasd_b =font3.render('WASD - move buttons. You can only go up and down the stairs', True, (255,0,0))
+space_b = font3.render('Space - shoot button. You are a wizard who only knows one spell', True, (255, 0 , 0))
+e_b = font3.render('E - interaction button. open doors, collect keys, activate portals', True, (255,0,0))
+
+font4 = font.SysFont(('font/ariblk.ttf'), 150)
+done =font4.render('LEVEL DONE!', True, (0,255,0), (255, 100, 0))
+lose = font4.render('YOU LOSE!', True, (255, 0 , 0), (245, 222, 179))
+pausa = font4.render('PAUSE', True, (255,0,0), (245, 222, 179))
 
 # Создание списков и групп спрайтов (перенести в функцию перезапуска
 blocks_r = []
