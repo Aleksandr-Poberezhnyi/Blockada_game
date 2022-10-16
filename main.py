ac9d1b4ccd522ef4809a8be9d215f1afe7f9a151
Dedyshka_95 <99130828+Aleksandr-Poberezhnyi@users.noreply.github.com>
2022-10-16 16:24:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -161,7 +161,17 @@ nothing = "images/nothing.png"
 power = "images/mana.png"
 widght = 1280
 height = 720
-#  Музыка
+
+mixer.init()
+
+fire_s = mixer.Sound('sounds/fire.ogg')
+kick = mixer.Sound('sounds/kick.ogg')
+k_up = mixer.Sound('sounds/k_coll.ogg')
+c_coll = mixer.Sound('sounds/c_coll.ogg')
+d_o = mixer.Sound('sounds/lock.ogg')
+tp = mixer.Sound('sounds/teleport.ogg')
+click = mixer.Sound('sounds/click.ogg')
+cst_o = mixer.Sound('sounds/chest.ogg')
 
 # добавляем текст в игре
 
@@ -218,7 +228,6 @@ done =font4.render('LEVEL DONE!', True, (0,255,0), (255, 100, 0))
 lose = font4.render('YOU LOSE!', True, (255, 0 , 0), (245, 222, 179))
 pausa = font4.render('PAUSE', True, (255,0,0), (245, 222, 179))
 
-# Создание списков и групп спрайтов (перенести в функцию перезапуска
 blocks_r = []
 blocks_l = []
 coins = []
@@ -438,7 +447,49 @@ if sprie.collide_rect(hero, chest) and k_chest == True:
 # движение камеры
     display.update()
     clock.tick(FPS)
-#Функция меню
+def menu():
+
+    menu = True
+    mixer.music.load("sounds/menu")
+    mixer.music.play()
+
+    while menu:
+
+        for e in event.get():
+            if e.type == QUIT:
+                menu = False
+
+        time.delay(15)
+        pos_x, pos_y = mouse.get_pos()
+
+        win.blit(bg, (0, 0))
+        win.blit(gname, (320, 70))
+
+        btn_start.draw(15, 5)
+        btn_control.draw(10, 5)
+        btn_exit.draw(37, 5)
+
+        for e in event.get():
+            if btn_start.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                menu = False
+                res_pos()
+                lvl_1()
+            
+            if btn_control..rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                menu = False
+                rules()
+
+            if btn_exit.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                clck.play()
+                menu = False
+                a = 0
+
+            if e.type == QUIT:
+                menu = False
+                a = 0
+        display.update()
 def rules():
     rule = True
     mixer.music.stop()
@@ -472,7 +523,50 @@ def rules():
                 rule = False
 
         display.update()
-# Пауза в игре
+def pause(): # nayзa    
+    stop = True
+
+    mixer.music.stop() # зупинка вси звуKİв
+
+    while stop:
+
+        for e in event.get(): # закpиваемо вiкно гри
+            if e.type == QUIT: 
+                stop = False
+
+        time.delay(15)
+
+        win.fill((0, 0, 0))
+        win.blit(pausa, (440, 200)) 
+        # відображення кнопок
+        btn_continue.draw(58, 5)
+        btn_restart.draw(60, 5)
+        btn_menu.draw(0, 5)
+
+        pos_x, pos_y= mouse.get_pos()
+
+        for e in event.get():
+            # продовжуємо ГРУ
+            if btn_continue.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN: 
+                click.play()
+                stop = False
+                mixer.music.stop()
+                lvl_1()
+            # перезапуск рiвня
+            if btn_restart.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                res_pos()
+                mixer.music.stop()
+                stop = False
+                1v1_1()
+            # меню
+            if btn_menu.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                mixer.music.stop()
+                stop = False 
+                game = False
+                menu()
+        display.update()
 def restart(): # пepeзanyck
 
     over = True
@@ -505,7 +599,7 @@ def restart(): # пepeзanyck
                 over = False
                 res_pos()
                 lvl_1()
-            # меню
+            
             if btn_menu.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN: 
                 click.play()   
                 over = False               
